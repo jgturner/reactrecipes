98612d1fd83dfdad8e3aa9dbac02dbe82aa103da
jgturner <jgturner.29@gmail.com>
2024-04-21 16:26:40 -0400

    verifying changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 export function Header() {
   return (
-    <header className="text-center">
+    <header className="text-center flex items-center justify-center my-5 gap-2">
       <img className="w-24" src="./imgs/logo.png" alt="react logo" />
-      <h1>
-        <span className="family">React</span> Recipes
-      </h1>
+      <h1 className="text-3xl font-bold">React Recipes</h1>
     </header>
   );
 }
